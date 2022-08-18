4c9aa2ed4500fb2dacd74bd4ff11f0be60555485
David Ouellette <david.ouellette12@protonmail.com>
2022-08-18 07:32:01 -0400

    update env variables

--- index.js
+++ index.js
@@ -6,16 +6,10 @@ const PORT = 4001
 const Schema = mongoose.Schema
 const ObjectId = Schema.ObjectId
 
-const credentials = "./X509-cert-6375362467439856284.pem"
-
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://dodevcluster.r8asi.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
-{
-    sslKey: credentials,
-    sslCert: credentials 
-})
+mongoose.connect(process.env.MONGODB_URI)
 .catch(err => console.error(err))
 
 
